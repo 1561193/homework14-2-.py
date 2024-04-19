d3292ce9988101e3a6a318f7af0765c8845912aa
1561193 <164223652+1561193@users.noreply.github.com>
2024-04-19 12:51:43 +0300

    Add files via upload

--- homework14(2).py
+++ homework14(2).py
@@ -1,18 +1,33 @@
-def test_(*args):
-    print(test_)
-    print('тип args:', type(args))
+def test_info(*args, **kwargs):
     print(args)
     for i, arg in enumerate(args):
-        print('позиция:', i, arg)
+        print('Позиционный параметр:', i, arg )
+    print(kwargs)
+    for key, value in kwargs.items():
+            print('Именованный аргумент:', key, '=', value)
 
 
-test_(4, 'как дела?', True)
+test_info(59, 'Андрей', True, пёс="Умка", порода= 'Алабай')
+
+# Ещё один вариант
+
+def test(*args, names_autor="Andr", **kwargs):
+    print('Данные:', kwargs)
+    for key, n in kwargs.items():
+        print(key, n)
+    print(args)
 
 
-def fac(n):
+test('Пример использования:', 1, 2, 3, 4, names_author='Andr', name='Andrey', course='Pyton')
+
+
+def factorial(n):
     if n == 1:
         return 1
-    return fac(n-1) * n
+    a = factorial(n=n -1)
+    return n * a
+print(factorial(10))
 
 
-print(fac(6))
+
+
